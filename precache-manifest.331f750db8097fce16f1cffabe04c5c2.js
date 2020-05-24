11dc6dcf02ba9f2a17af69c117df6db5e19c1e10
tarunsonie <tarunsonie@users.noreply.github.com>
2020-05-24 15:24:25 +0530

    Updates

---
+++ precache-manifest.331f750db8097fce16f1cffabe04c5c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237c94344ef926b358ba3102e402dbcb",
+    "url": "/Robotfriends/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b337faac19b05c27a192",
+    "url": "/Robotfriends/static/css/main.637ac540.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Robotfriends/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robotfriends/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b337faac19b05c27a192",
+    "url": "/Robotfriends/static/js/main.69407525.chunk.js"
+  },
+  {
+    "revision": "b01683e13fe1325196b1",
+    "url": "/Robotfriends/static/js/runtime-main.7f7adda7.js"
+  }
+]);
